ac13c26d6f66ab0859058e0f4c3485cd95cb1208
wil <mcpc2003twq@gmail.com>
2016-10-29 12:01:02 +0800

    39 建立 ARController 並練習將 WebFormViewEngine 移除

--- MVC5Course/Global.asax.cs
+++ MVC5Course/Global.asax.cs
@@ -18,6 +18,10 @@ namespace MVC5Course
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //減少view搜尋的格式,只搜尋razor
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
